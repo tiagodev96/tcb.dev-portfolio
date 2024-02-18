6480661e93ca5b8aaba7ef7f1a3f4bff1d4b9056
Tiago Batista <tiago.dev96@gmail.com>
2024-02-17 23:08:53 -0300

    feat: footer implementations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer, Header } from "@/components";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Header />
         <>{children}</>
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import Link from "next/link";
-import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
+import { Facebook, Instagram, Linkedin, Send, Twitter } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Logo } from "./components/Logo";
+import { MessageInput } from "./components/MessageInput";
+import { QuickPages } from "./components/QuickPages";
+import { Copyright } from "./components/Copyright";
 
 export function Footer() {
   return (
@@ -31,75 +34,30 @@ export function Footer() {
             trabalho, conecte-se e juntos vamos criar algo extraordinário!
           </p>
           <div className="flex justify-center mt-4 space-x-4 lg:mt-8">
-            <Link href={""}>
-              <Facebook className="text-blue-500" />
+            <Link href="https://twitter.com/tiagocb96" target="_blank">
+              <Twitter className="text-100 transition-all hover:text-sky-300" />
             </Link>
-            <Link href={""}>
-              <Twitter className="text-sky-300" />
+            <Link
+              href="https://www.instagram.com/tiagocbatista"
+              target="_blank"
+            >
+              <Instagram className="text-100 transition-all hover:text-pink-500" />
             </Link>
-            <Link href={""}>
-              <Instagram className="text-pink-500" />
-            </Link>
-            <Link href={""}>
-              <Linkedin className="text-blue-400" />
+            <Link href="https://www.linkedin.com/in/tiagocb96/" target="_blank">
+              <Linkedin className="text-100 transition-all hover:text-blue-400" />
             </Link>
           </div>
         </div>
+
         <div className="justify-between w-full mt-4 text-center lg:flex">
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-              Quick Links
-            </h2>
-            <ul className="mb-8 space-y-2 text-sm list-none">
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 1
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 2
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 3
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 4
-                </Link>
-              </li>
-            </ul>
+            <MessageInput />
           </div>
+
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-              Quick Links
-            </h2>
-            <ul className="mb-8 space-y-2 text-sm list-none">
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 1
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 2
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 3
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 4
-                </Link>
-              </li>
-            </ul>
+            <QuickPages />
           </div>
+
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
             <h2 className="mb-2 font-bold tracking-widest text-gray-100">
               Quick Links
@@ -129,10 +87,9 @@ export function Footer() {
           </div>
         </div>
       </div>
+
       <div className="flex justify-center -mt-12">
-        <p className="text-center text-white pb-2">
-          @{new Date().getFullYear()} All rights reserved by your website.
-        </p>
+        <Copyright />
       </div>
     </footer>
   );
